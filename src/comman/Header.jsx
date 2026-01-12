20b47ea6c262cbd475e281c47dacde602cedb634
yug patel <yugjpatel412@gmail.com>
2026-01-12 14:35:12 +0530

    first commit

--- src/comman/Header.jsx
+++ src/comman/Header.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 import logo from "/img/olynto-logo.png";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="header-container">
@@ -13,21 +15,37 @@ const Header = () => {
           <img src={logo} alt="Olynto Logo" />
         </div>
 
+        {/* HAMBURGER */}
+        <div
+          className={`hamburger ${menuOpen ? "open" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
         {/* NAV */}
-        <nav className="nav">
-          <NavLink to="/home" className={({ isActive }) => isActive ? "active" : ""}>
+        <nav className={`nav ${menuOpen ? "show" : ""}`}>
+          <NavLink to="/home" onClick={() => setMenuOpen(false)}>
             Home
           </NavLink>
 
-          <NavLink to="/services" className={({ isActive }) => isActive ? "active" : ""}>
-            service
+          <NavLink to="/services" onClick={() => setMenuOpen(false)}>
+            Services
           </NavLink>
-         
-    <NavLink to="/casestudy">Case Studies</NavLink>
-        
-  
-           <NavLink to="/about-us">About Us</NavLink>
-             <NavLink to="/contact-us">Contact & Strategy Call</NavLink> 
+
+          <NavLink to="/casestudy" onClick={() => setMenuOpen(false)}>
+            Case Studies
+          </NavLink>
+
+          <NavLink to="/about-us" onClick={() => setMenuOpen(false)}>
+            About Us
+          </NavLink>
+
+          <NavLink to="/contact-us" onClick={() => setMenuOpen(false)}>
+            Contact & Strategy Call
+          </NavLink>
         </nav>
 
       </div>
